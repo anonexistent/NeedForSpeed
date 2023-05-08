403ebf5532dd93debb1bf1704a4063e329026408
anonexistent <wwwzlx86@gmail.com>
2023-05-08 18:49:34 +0500

    Добавьте файлы проекта.

---
+++ NeedForSpeed/Subject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+/// <summary>
+/// Description player
+/// </summary>
+
+namespace NeedForSpeed
+{
+    internal class Subject
+    {
+        public bool IsAlive { get; set; }
+        //public bool[] Challenges { get { return Challenges; } set { Challenges = value; 
+        //                                                                    //MakeInfo(); 
+        //                                                                } }
+
+        public string Info { get; set; }
+
+        public Subject()
+        {
+            IsAlive = true;
+            //Challenges = new bool[4] { false, false, false, false };
+            Info = string.Empty;
+        }
+
+        //void MakeInfo()
+        //{
+        //    if (challenges.Contains(false))
+        //    {
+        //        IsAlive = false;
+        //        Info = "die ☺";
+        //        for (int i = 0; i < Challenges.Length; i++) if (!Challenges[i]) Info += $" (test № {i+1})";
+        //    }
+
+        //}
+    }
+}
